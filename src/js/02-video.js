486fcb1f1241305d5ab9c325a362c154d810831f
Stef712 <148722481+Stef712@users.noreply.github.com>
2024-04-24 17:44:04 +0300

    finall

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -22,17 +22,13 @@ const timeStart = localStorage.getItem('videoplayer-current-time');
 if (timeStart) {
   player
     .setCurrentTime(timeStart)
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
+    .then(function (seconds) {})
     .catch(function (error) {
       switch (error.name) {
         case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
           break;
 
         default:
-          // some other error occurred
           break;
       }
     });
